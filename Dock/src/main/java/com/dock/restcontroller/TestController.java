fb6fbdb0186ec2219824e204c74bb5586ca5a992
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-04 21:46:49 +0530

    mvn clean package

--- Dock/src/main/java/com/dock/restcontroller/TestController.java
+++ Dock/src/main/java/com/dock/restcontroller/TestController.java
@@ -1,9 +1,11 @@
 package com.dock.restcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/testController")
 public class TestController 
 {
 	// EndPoint to display userName,
